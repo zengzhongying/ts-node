f62b6ef7e25079d3163bdf8bd1799cb7684bd15b
zy.zeng <1363421830@qq.com>
2020-05-08 10:36:19 +0800

    區分大小寫

--- db/dbFamily/member/memberModel.js
+++ db/dbFamily/member/memberModel.js
@@ -1,6 +1,6 @@
 // MemberModel.js file
 const connection = require('../../connection');
-const MemberSchema = require('./MemberSchema');
+const MemberSchema = require('./memberSchema');
 
 let MemberModel = connection("dbFamily").model('Member', MemberSchema);
 
