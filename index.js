b041ff6f15e80f59764cc51480f58f1a294b5575
zy.zeng <1363421830@qq.com>
2020-05-21 16:20:44 +0800

    升級為https

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const https = require('https')   //升級為https
 const app = express();
+const fs = require('fs');
 
 //引入路由模块
 const member = require('./routes/member.js');
@@ -15,7 +17,11 @@ app.use(cors());
 app.use('/member', member)
 app.use('/ssList', ssList)
 
-app.listen(8111, () => { console.log("服務啟動") });
+// app.listen(8111, () => { console.log("服務啟動") });
+https.createServer({
+  key: fs.readFileSync('./module/2_nnnight.club.key'),//证书文件的存放目录
+  cert: fs.readFileSync('./module/1_nnnight.club_bundle.crt')
+}, app).listen(443);//https默认是443端口
 
 // 根据uid获取用户信息
 // http://statistics.pandadastudio.com/player/simpleInfo?uid=311484178
